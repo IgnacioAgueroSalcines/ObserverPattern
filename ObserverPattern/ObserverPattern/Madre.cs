4127e867765167d8e34d839a00baafb432399b05
Ignacio Agüero Salcines <ias61@alumnos.unican.es>
2016-11-20 17:53:53 +0100

    inicializacion del proyecto

---
+++ ObserverPattern/ObserverPattern/Madre.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObserverPattern
+{
+    public class Madre
+    {
+        private String nombre { get; set; }
+        private Padre esposo { get; set; }
+        private IList<Hijo> hijos { get; set; }
+    }
+}
---
+++ ObserverPattern/ObserverPattern/Padre.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObserverPattern
+{
+    public class Padre
+    {
+        private String nombre { get; set; }
+        private Madre esposa { get; set; }
+        private IList<Hijo> hijos { get; set; }
+
+    }
+}
